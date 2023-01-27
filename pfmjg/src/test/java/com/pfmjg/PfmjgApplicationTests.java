2d3e4c4710510b95b668c9e6b09d3bf8b77a9db1
Endo99 <wellington.endo@edu.unifil.br>
2023-01-27 08:52:12 -0300

    Começo e salvando no repositório

---
+++ pfmjg/src/test/java/com/pfmjg/PfmjgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfmjg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmjgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
